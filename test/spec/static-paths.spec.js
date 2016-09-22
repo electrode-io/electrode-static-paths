f525be5139e221f9222dd6d6ef6e409c5e2e6df2
Joel Chen <xchen@walmartlabs.com>
2016-09-21 22:23:42 -0700

    run test server at randomly assigned port

--- test/spec/static-paths.spec.js
+++ test/spec/static-paths.spec.js
@@ -12,21 +12,22 @@ describe("static-paths", function () {
     let server;
     const verifyServerStatic = (s) => {
       server = s;
+      const port = server.info.port;
       return Promise.resolve()
-        .then(() => sa.get("http://localhost:3000/html/hello.html")
+        .then(() => sa.get(`http://localhost:${port}/html/hello.html`)
           .then((resp) => {
             assert(resp, "Server didn't return response");
             assert(_.includes(resp.text, "Hello Test!"),
               "response not contain expected string");
           }))
-        .then(() => sa.get("http://localhost:3000/images/hello.svg")
+        .then(() => sa.get(`http://localhost:${port}/images/hello.svg`)
           .then((resp) => {
             expect(resp.statusCode).to.equal(200);
             expect(resp.type).to.equal("image/svg+xml");
             const body = resp.body.toString();
             expect(body).to.equal(`<circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />\n`);
           }))
-        .then(() => sa.get("http://localhost:3000/js/hello.js")
+        .then(() => sa.get(`http://localhost:${port}/js/hello.js`)
           .then((resp) => {
             expect(resp.statusCode).to.equal(200);
             expect(resp.type).to.equal("application/javascript");
@@ -34,6 +35,11 @@ describe("static-paths", function () {
     };
 
     const config = {
+      connections: {
+        default: {
+          port: 0
+        }
+      },
       plugins: {
         electrodeStaticPaths: {
           options: {
